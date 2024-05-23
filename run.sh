90548a1d18b6828a2a37dba8fce55ad9b8ec8cb6
Nick Schneider <nickschneider101@gmail.com>
2024-05-22 21:43:58 -0700

    Implement run.sh to run all the docker compose files together

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
 # Add command(s) to run all of the docker compose files together
+docker compose \
+    -f docker/docker-compose.yaml \
+    -f docker/immich/docker-compose.yaml \
+    -f docker/spotify/docker-compose.yaml \
+    up
